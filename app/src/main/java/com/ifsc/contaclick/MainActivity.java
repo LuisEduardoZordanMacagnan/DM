2fb61ff5c0b516752f8cb116709c7df2c4a0a6b8
= <=>
2025-06-05 21:45:08 -0300

    Lista com INSERT, UPDATE e DELETE

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,8 +1,15 @@
 package com.ifsc.contaclick;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,9 +18,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    int indice = 0;
+    SQLiteDatabase db;
+    Button salvar;
+    TextView texto;
+    ListView listView;
+    ArrayList<Nota> notas = new ArrayList<Nota>();
+    Integer id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +40,83 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Button botao = findViewById(R.id.button);
-        TextView text = findViewById(R.id.indice);
+        salvar = findViewById(R.id.salvar);
+        texto = findViewById(R.id.texto);
+        listView = findViewById(R.id.view);
 
-        botao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                indice++;
-                text.setText(String.valueOf(indice));
+        db = openOrCreateDatabase("notas", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS NOTAS (" +
+                "ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                "TEXTO TEXT)");
+
+        salvar.setOnClickListener(b ->{
+            if (id!=null){
+                atualizaNota(id, texto.getText().toString());
+            } else {
+                insereNota(texto.getText().toString());
             }
+
+            texto.setText("");
+            id = null;
+            loadNotas();
         });
+
+        listView.setOnItemLongClickListener((adapterView, view, i, l) -> {
+            deletarNota(notas.get(i).getId());
+
+            loadNotas();
+
+            return false;
+        });
+
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            Nota nota = notas.get(i);
+
+            texto.setText(nota.getTexto());
+            id = nota.getId();
+
+        });
+
+        loadNotas();
+    }
+
+    void loadNotas(){
+        notas.clear();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM notas", null);
+        cursor.moveToFirst();
+
+        int cId = cursor.getColumnIndex("ID");
+        int cTexto = cursor.getColumnIndex("TEXTO");
+        while (!cursor.isAfterLast()){
+            notas.add(new Nota(cursor.getInt(cId), cursor.getString(cTexto)));
+            cursor.moveToNext();
+        }
+
+        AdapterNota adapter = new AdapterNota(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                notas);
+
+        listView.setAdapter(adapter);
+
+        cursor.close();
+    }
+
+    Long insereNota(String insert){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("TEXTO", insert);
+
+        return db.insert("notas", null, contentValues);
+    }
+
+    void atualizaNota(int id, String update){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("TEXTO", update);
+
+        db.update("notas", contentValues, "id=?", new String[]{Integer.toString(id)});
+    }
+
+    void deletarNota(int delete){
+        db.delete("notas", "ID = ?", new String[] {Integer.toString(delete)});
     }
 }
---
+++ app/src/main/java/com/ifsc/contaclick/Nota.java
@@ -0,0 +1,27 @@
+package com.ifsc.contaclick;
+
+public class Nota {
+    private int id;
+    private String texto;
+
+    public Nota(int id, String texto) {
+        this.id = id;
+        this.texto = texto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
