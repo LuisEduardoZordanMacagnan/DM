6c3e0c1816ccd67ecde4ae0349b8a1ebca17cfd3
LuisEduardoZordanMacagnan <luiseduardozordanmacagnan@gmail.com>
2025-07-03 20:32:01 -0300

    Agora com um mapa

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -6,6 +6,8 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +19,11 @@ import androidx.core.app.ActivityCompat;
 import com.google.android.gms.location.*;
 import com.google.android.gms.location.FusedLocationProviderClient;
 
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
@@ -25,10 +32,32 @@ public class MainActivity extends AppCompatActivity {
     private LocationRequest locationRequest;
     private LocationCallback locationCallback;
 
+    private MapView map;
+    private boolean track = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Configuration.getInstance().setUserAgentValue(this.getPackageName());
+
+        Button home = findViewById(R.id.viewHome);
+
+        home.setOnClickListener(v->{
+            track = true;
+        });
+
+        map = findViewById(R.id.map);
+
+        map.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                track = false;
+                return false;
+            }
+        });
+        map.getController().setZoom(18);
 
         txtCoordenadas = findViewById(R.id.txtCoordenadas);
         Button btnGetLocation = findViewById(R.id.btnGetLocation);
@@ -61,10 +90,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void getLocation() {
         //checa se a permissão de localização foi concedida usando o ActivityCompat do AndroidX
-        //getPackageManager().checkPermission(permission, getPackageName()); equivalente apos api 23
         if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) || (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) != PackageManager.PERMISSION_GRANTED ){
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     LOCATION_PERMISSION_REQUEST_CODE);
             return;
         }
@@ -76,9 +105,25 @@ public class MainActivity extends AppCompatActivity {
     public final LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(@NonNull Location location) {
-            txtCoordenadas.setText("Latitude: " + location.getLatitude() + "\nLongitude: " + location.getLongitude());
+            double latitude = location.getLatitude(), longitude = location.getLongitude();
+            txtCoordenadas.setText("Latitude: " + latitude + "\nLongitude: " + longitude);
+            showLocalizacao(latitude, longitude);
         }
     };
+
+    public void showLocalizacao(double latitutde, double longitude){
+        map.getOverlays().clear();
+        GeoPoint userLocation = new GeoPoint(latitutde, longitude);
+        Marker marker = new Marker(map);
+        marker.setTitle("Usuário");
+        marker.setPosition(userLocation);
+        if ( track ) {
+            map.getController().setCenter(userLocation);
+        }
+        map.getOverlays().add(marker);
+        map.invalidate();
+
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
